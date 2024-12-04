372bc50af98cc21eb51d0c359220c3f2a73be4c6
[Ahmed390650] <ahmed_elsayed_9961@yahoo.com>
2024-12-04 12:35:34 +0200

    env

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
-export const Lusitana = Inter({ weight: ["400", "700"], subsets: ["latin"] });
+export const lusitana = Inter({ weight: ["400", "700"], subsets: ["latin"] });
